e80aad3036b787d6326b0cd943c073ebb593bc09
artem <ashepe@softserveinc.com>
2022-09-19 21:55:34 +0200

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
@@ -8,6 +9,11 @@ import store from './redux';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='test' element={<div>Test route</div>} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
 );
